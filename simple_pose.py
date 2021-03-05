a56c5caf49e7a763ae5bad2d60631d7a9e1d9fa4
Travis Riggs <travis@fastem.com>
2021-03-05 14:30:50 -0500

    DEBUG: Debugging simple pose script

--- simple_pose.py
+++ simple_pose.py
@@ -19,13 +19,42 @@ from PIL import ImageDraw
 import numpy as np
 import os
 
+# My additions
+import cv2
+
+# DEBUG
+import ipdb
+
 os.system('wget https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/'
           'Hindu_marriage_ceremony_offering.jpg/'
           '640px-Hindu_marriage_ceremony_offering.jpg -O /tmp/couple.jpg')
 pil_image = Image.open('/tmp/couple.jpg').convert('RGB')
+# pil_image = Image.open('/tmp/couple.jpg')
+# pil_image.show()
+
 engine = PoseEngine(
-    'models/mobilenet/posenet_mobilenet_v1_075_481_641_quant_decoder_edgetpu.tflite')
+        'models/mobilenet/posenet_mobilenet_v1_075_481_641_quant_decoder_edgetpu.tflite')
 poses, inference_time = engine.DetectPosesInImage(pil_image)
+print('Inference time: %.f ms' % (inference_time * 1000))
+
+for pose in poses:
+    if pose.score < 0.4: continue
+    print('\nPose Score: ', pose.score)
+    for label, keypoint in pose.keypoints.items():
+        print('  %-20s x=%-4d y=%-4d score=%.1f' %
+              (label.name, keypoint.point[0], keypoint.point[1], keypoint.score))
+
+# With opencv
+print('With OpenCV:')
+ipdb.set_trace()
+cv_image = cv2.imread('/tmp/couple.jpg')
+# The lines below, converting from CV to PIL, work properly
+# cv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
+# pil_image = Image.fromarray(cv_image)
+# pil_image.show()
+# poses, inference_time = engine.DetectPosesInImage(pil_image)
+poses, inference_time = engine.DetectPosesInCVImage(cv_image)
+
 print('Inference time: %.f ms' % (inference_time * 1000))
 
 for pose in poses:
